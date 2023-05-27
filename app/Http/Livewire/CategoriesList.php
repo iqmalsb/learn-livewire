372b8fd20b6b6c2da28974d9b40b0aee543c14fb
Iqmal Shamsul Bahari <iqmalshamsul96@gmail.com>
2023-05-28 03:12:57 +0800

    lesson 04/17 create new category modal

--- app/Http/Livewire/CategoriesList.php
+++ app/Http/Livewire/CategoriesList.php
@@ -4,18 +4,49 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Category;
+use Illuminate\Support\Str;
 use Livewire\WithPagination;
 
 class CategoriesList extends Component
 {
     use WithPagination;
 
+    public Category $category;
+
+    public bool $showModal = false;
+
+    protected function rules(): array 
+    {
+        return [
+            'category.name' => ['required', 'string', 'min:3'],
+            'category.slug' => ['nullable', 'string'],
+        ];
+    }
+
+    public function updatedCategoryName()
+    {
+        $this->category->slug = Str::slug($this->category->name);
+    }
+    
+    public function openModal()
+    {
+        $this->showModal = true;
+
+        $this->category = new Category();
+    }
+
+    public function save() 
+    {
+        $this->validate();
+        $this->category->save();
+
+        $this->reset('showModal');
+    } 
+
     public function render()
     {
-        $categories = Category::paginate(10);
-
         return view('livewire.categories-list', [
-            'categories' => $categories,
+            'categories' => Category::paginate(10),
         ]);
     }
 }
