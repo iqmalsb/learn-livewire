8cd7bd523c9ab374894d699cae12d5140bea60c0
Iqmal Shamsul Bahari <iqmalshamsul96@gmail.com>
2023-05-28 00:13:51 +0800

    lesson 02/07 register password show/hide/strength

--- app/Http/Livewire/RegisterPasswords.php
+++ app/Http/Livewire/RegisterPasswords.php
@@ -2,10 +2,53 @@
 
 namespace App\Http\Livewire;
 
+use ZxcvbnPhp\Zxcvbn;
 use Livewire\Component;
 
 class RegisterPasswords extends Component
 {
+    public string $password = '';
+    public string $passwordConfirmation = '';
+    public int $strengthScore = 0;
+    public array $strengthLevels = [
+        1 => 'Very weak',
+        2 => 'Weak',
+        3 => 'Medium',
+        4 => 'Strong',
+    ];
+
+    public function updatedPassword($value)
+    {
+        $this->strengthScore = (new Zxcvbn())->passwordStrength($value)['score'];
+    }
+
+    public function generatePassword(): void
+    {
+        $lowercase = range('a', 'z');
+        $uppercase = range('A', 'Z');
+        $digits = range(0, 9);
+        $special = ['!', '@', '#', '$', '%', '^', '*'];
+        $chars = array_merge($lowercase, $uppercase, $digits, $special);
+        $length = 12;
+        do {
+            $password = array();
+
+            for ($i = 0; $i <= $length; $i++) {
+                $int = rand(0, count($chars) - 1);
+                $password[] = $chars[$int];
+            }
+        } while (empty(array_intersect($special, $password)));
+
+        $this->setPasswords(implode('', $password));
+    }
+
+    private function setPasswords($value): void
+    {
+        $this->password = $value;
+        $this->passwordConfirmation = $value;
+        $this->updatedPassword($value);
+    }
+
     public function render()
     {
         return view('livewire.register-passwords');
